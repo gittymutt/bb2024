81085b302f88dc66f918185577263f65cda4a44c
gittymutt <pedroschmitz@hotmail.com>
2022-01-28 22:03:22 -0500

    bio and footer

--- src/components/bio.js
+++ src/components/bio.js
@@ -39,12 +39,7 @@ const Bio = () => {
           Written by <strong>{author.firstName}</strong>
           {` `}
           {author?.description || null}
-          {` `}
-          {author?.twitter && (
-            <a href={`https://twitter.com/${author?.twitter || ``}`}>
-              You should follow them on Twitter
-            </a>
-          )}
+          
         </p>
       )}
     </div>
