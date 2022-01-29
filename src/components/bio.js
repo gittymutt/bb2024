f4f8f10a73d38d4d088b71d927d165765a5a4efc
gittymutt <pedroschmitz@hotmail.com>
2022-01-28 21:33:55 -0500

    Change bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -37,14 +37,10 @@ const Bio = () => {
       {author?.firstName && (
         <p>
           Written by <strong>{author.firstName}</strong>
-          {` `}
+          {`, `}
           {author?.description || null}
-          {` `}
-          {author?.twitter && (
-            <a href={`https://twitter.com/${author?.twitter || ``}`}>
-              You should follow them on Twitter
-            </a>
-          )}
+          
+          
         </p>
       )}
     </div>
